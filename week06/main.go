d396ccb69fc32fbe3f056a7d4e16176f9eaf1278
cha80537@gmail.com <cha80537@gmail.com>
2024-10-04 14:47:32 +0900

    v0.5 conversion

--- week06/main.go
+++ week06/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
+	i := 13              // var i int = 13
+	var f float64 = 12.9 // f := 12.9
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	// fmt.Printf("%d * %f * %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f * %f\n", i, f, float64(i)*f) // 강제변환 conversion
-	fmt.Println(reflect.TypeOf(i))                   // i는 바뀌지 않음
+	// fmt.Printf("%d * %f * %f\n", i, f, float64(i)*f)   // 강제변환 conversion 실수타입
+	fmt.Printf("%d * %f * %d\n", i, f, i*int(f))      // 강제변환 conversion 정수타입
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f)) // i는 바뀌지 않음
 }
