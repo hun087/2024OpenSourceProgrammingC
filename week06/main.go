9818f0ae85592dc74a39035a81793b5a56f7d631
cha80537@gmail.com <cha80537@gmail.com>
2024-10-04 15:49:10 +0900

    v0.7 Zero value

--- week06/main.go
+++ week06/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-func main() {
-	i := 13              // var i int = 13
-	var f float64 = 12.9 // f := 12.9
-	c1 := 'Z'            // 90
-	c2 := '김'            //44608, home. Unicode
+// zero values
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	// fmt.Printf("%d * %f * %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
-	// fmt.Printf("%d * %f * %f\n", i, f, float64(i)*f)   // 강제변환 conversion 실수타입
-	fmt.Printf("%d * %f * %d\n", i, f, i*int(f)) // 강제변환 conversion 정수타입
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2)) // i는 바뀌지 않음
+func main() {
+	var f float64
+	var i int
+	var b bool
+	var s string
+
+	my_school_account := 5
+	println(my_school_account)
+
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero values
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f <= float64(i), "\n") // comparison (true/false)
+
 }
