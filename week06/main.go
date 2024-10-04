40022359048152087c9338c0956f652f8ec82f8f
cha80537@gmail.com <cha80537@gmail.com>
2024-10-04 14:31:41 +0900

    v0.4 conversion

--- week06/main.go
+++ week06/main.go
@@ -9,7 +9,7 @@ func main() {
 	var i int = 13
 	f := 12.9
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	// fmt.Printf("%d * %f * %f\n", i, f, i*f) //.\main.go:14:37: invalid operation: i * f (mismatched types int and float64)
+	// fmt.Printf("%d * %f * %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
 	fmt.Printf("%d * %f * %f\n", i, f, float64(i)*f) // 강제변환 conversion
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Println(reflect.TypeOf(i))                   // i는 바뀌지 않음
 }
