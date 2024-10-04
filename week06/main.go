0656721014d10a6c43d04909c95e700d1cf96125
cha80537@gmail.com <dkdl458056@gmail.com>
2024-10-04 14:26:45 +0900

    v0.4 conversion

--- week06/main.go
+++ week06/main.go
@@ -1,9 +1,15 @@
 package main
 
-func main() {
-	i := 13
-	f := 12.9
+import (
+	"fmt"
+	"reflect"
+)
 
-	fmt.printf("value i : %d, value f : %d\n", i, f)
-	fmt.printf("%d * %f = %f\n", i, f, i*f)
+func main() {
+	var i int = 13
+	f := 12.9
+	fmt.Printf("value i : %d, value f : %f\n", i, f)
+	// fmt.Printf("%d * %f * %f\n", i, f, i*f) //.\main.go:14:37: invalid operation: i * f (mismatched types int and float64)
+	fmt.Printf("%d * %f * %f\n", i, f, float64(i)*f) // 강제변환 conversion
+	fmt.Println(reflect.TypeOf(i))
 }
