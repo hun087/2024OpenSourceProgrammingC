c37c31ba7f72d459f5d1cede7f3e4d9207ddd73e
cha80537@gmail.com <cha80537@gmail.com>
2024-10-11 14:27:52 +0900

    v1.11 Stdin

--- week07/main.go
+++ week07/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	name, err := in.ReadString('\n')
+	in := bufio.NewReader(os.Stdin)  // 입력받는 방법1
+	name, err := in.ReadString('\n') // 입력받는 방법2
 	fmt.Printf("Input your name : ")
 	fmt.Println(name)
 	fmt.Println(err)
