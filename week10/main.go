38ab91ef1e2d443e8350970597844d354abb06bb
cha80537@gmail.com <cha80537@gmail.com>
2024-11-01 15:37:02 +0900

    v3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -1,31 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	var isPrime bool = true
+func isPrime(n int) bool {
+	//var isPrime bool = true
 	if n <= 1 {
-		isPrime = false
+		//isPrime = false
+		return false
 	} else if n == 2 {
-		isPrime = true
+		//isPrime = true
+		return true
 	} else if n%2 == 0 {
-		isPrime = false
+		//isPrime = false
+		return false
 	} else {
 		j := 3
-		// for j <= int(math.Sqrt(float64(n))) {
 		for j*j <= n {
 			if n%j == 0 {
-				isPrime = false
-				break
+				//isPrime = false
+				//break
+				return false
 			}
 			fmt.Printf("%d ", j)
 			j = j + 2
 		}
 	}
+	return true
+}
 
-	if isPrime {
+func main() {
+	fmt.Print("Input number : ")
+	in := bufio.NewReader(os.Stdin)
+	i, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
+	n, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if isPrime(n) {
 		fmt.Printf("%d is prime number.", n)
 	} else {
 		fmt.Printf("%d is NOT prime number.", n)
